3cbfe661d2115f1441b3197f65c2dec42bd9d566
OriGuez <orig200@gmail.com>
2024-05-30 18:20:52 +0300

    Login

---
+++ project-web/src/Login.js
@@ -0,0 +1,109 @@
+import React, { useState, useRef } from 'react';
+import './App.css';
+//function AppLogin({usersList, setLoggedUser})
+function AppLogin() {
+  const usernameInput = useRef();
+  const passwordInput = useRef();
+  const logged = null;
+
+  const validateUsername = (event) => {
+    const val = event.target.value;
+    if (val.length < 1) {
+        usernameInput.current.classList.add('invalidInput');
+        usernameInput.current.setCustomValidity('Username must contain at least 1 character');
+      return;
+    }
+
+    // logged = (usersList.find (user => user.usernameInput === usernameInput  ))
+    // if(logged == null )
+    //     {
+    //      usernameInput.current.classList.add('invalidInput');
+    //      usernameInput.current.setCustomValidity('Username not found');
+    //     }
+  };
+
+    const validatePassword = (event) => {
+    const pwval = event.target.value;
+    let hasNonNumeric = false;
+
+    if (pwval.length < 8) {
+      passwordInput.current.classList.add('invalidInput');
+      passwordInput.current.setCustomValidity('Password must contain at least 8 characters');
+      return;
+    }
+
+    for (let i = 0; i < pwval.length; i++) {
+      const charCode = pwval.charCodeAt(i);
+      if (charCode < 48 || charCode > 57) {
+        hasNonNumeric = true;
+        break;
+      }
+    }
+
+    if (!hasNonNumeric) {
+      passwordInput.current.classList.add('invalidInput');
+      passwordInput.current.setCustomValidity('Password must contain at least one non-numeric character.');
+    } else {
+      passwordInput.current.classList.remove('invalidInput');
+      passwordInput.current.setCustomValidity('');
+    }
+  }
+    // const signIn = (event) => {
+        
+    //     if (logged != null && logged.password === passwordInput)
+    //         {
+    //             setLoggedUser(logged)
+    //             //move to homepage with loggedUser data
+    //         }
+    //     else
+    //     {
+    //         loggged.current.classList.add ('invalid input');
+    //         logged.current.setCustomValidity('incorrect username or password, try again');
+    //         return;
+    //     }
+        
+
+    // }
+
+
+//   ;
+
+  return (
+    <div className="App login-page">
+      <div className="login-form">
+        <h1 className="logo">ViewTube</h1>
+        <form>
+          <div className="input-container">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              placeholder="Enter your username"
+              onChange={validateUsername}
+              ref={usernameInput}
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+              onChange={validatePassword}
+              ref={passwordInput}
+            />
+          </div>
+          <button type="submit">Sign in</button>
+        </form>
+        <a href="#">Forgot password?</a>
+      </div>
+      <div className="create-account">
+        <p>
+          New to ViewTube? <a href="#">Create an account</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default AppLogin;
